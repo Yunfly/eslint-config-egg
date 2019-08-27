ccce1f1ab797d0fae99944609e41b218fb41f10c
wanghx <whx89768@alibaba-inc.com>
2019-08-27 16:20:03 +0800

    fix: new file bug in vscode

--- typescript.js
+++ typescript.js
@@ -3,6 +3,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [ '@typescript-eslint' ],
+  parserOptions: {
+    createDefaultProgram: true,
+  },
   extends: [
     './index.js',
     './lib/rules/typescript.js',
